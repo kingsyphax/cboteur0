067d0793c83cea2e69dc5a52dd700823a7c3a842
Nicholas Ward <kingsyphax@berkeley.edu>
2016-01-11 15:04:29 -0800

    yay much game stuff

--- game.py
+++ game.py
@@ -10,6 +10,9 @@ NUM_SABOTEURS = {3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 3, 10: 4}
 GOALS = [] #COMPLETE
 
 def main():
+    def play_card(player_number, card):
+        pass
+
     DEBUG = False
 
     # GET NUMBER OF PLAYERS
@@ -77,7 +80,43 @@ def main():
         time.sleep(1)
         input("If you are player %d, press Enter" % current_player)
 
-        print("Your cards: %s" % ", ".join(player_cards[current_player]))
+        print("Your cards:")
+        for index, card in enumerate(player_cards[current_player]):
+            print("\t%d: %s" % (index + 1, card))
+
+        time.sleep(1)
+
+        thing_done = False
+        while not thing_done:
+            card_to_do = input("Select a card (name or number): ")
+
+            while card_to_do not in player_cards[current_player] and int(card_to_do) not in range(1, len(player_cards[current_player]) + 1):
+                card_to_do = input("Card not found. Select a card: ")
+
+            if card_to_do in player_cards[current_player]:
+                card_number = player_cards[current_player].find(card_to_do) + 1
+            else:
+                card_number = int(card_to_do)
+
+            card_name = player_cards[current_player][card_number - 1]
+            print(deck.info(card_name))
+
+            time.sleep(1)
+
+            to_do = input("What would you like to do (play, discard, cancel)? ").lower()
+            while to_do not in ("play", "discard", "cancel"):
+                to_do = input("Not recognized. What would you like to do? ").lower()
+
+            if to_do == "cancel":
+                pass # do nothing; will loop back around to selecting another card
+            else:
+                thing_done = True
+                deck.discard(card_name)
+                player_cards[current_player].remove(card_name)
+                player_cards[current_player].append(deck.draw())
+                if to_do == "play":
+                    play_card(current_player, card_name)
+
 
         time.sleep(1)
         input("Press Enter to continue")
