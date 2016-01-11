f661ee298e9c37dba082f5973c759387cade8e65
Nicholas Ward <kingsyphax@berkeley.edu>
2016-01-11 14:28:49 -0800

    players

--- deck.py
+++ deck.py
@@ -1,52 +1,55 @@
-import random.shuffle
+import random
+
 class Deck:
-	#self.goals = [""]
+    #self.goals = [""]
 
-	self.discard = []
+    def __init__(self):
+        """ 
+        List of cards and descriptions for Deck:
 
-	def __init__(self):
-		""" 
-		List of cards and descriptions for Deck:
+        Read Spec - Peeks at a goal to check if correct or not
+        (Rethink Plan - Changes 2 of the 3 goals to different random ones.)
+        Add Line - Adds a line of code (or n characters)
+        Modify Line - Modifies a line of code (or < n characters)
+        Dead Battery - Traps a player
+        Disconnected WiFi - Traps a player
+        Stack Overflow Down - Traps a player (special). Everyone else has a 1/2 chance of skipping their turn after playing card.
+        Charger - Removes a Dead Battery trap
+        Internet Cafe - Removes a Disconnected Wifi trap
+        Go to Soda - Removes a Dead Battery and a Disconnected Wifi trap 
+        Code Guide - Makes a single player immune to Stack Overflow Down effect
+        Stack Overflow Restored - Can not be skipped. Removes all effects of ongoing Stack Overflow Down.
+        (Compile/Run) - Runs the code. After 3? run cards are played total, the game ends and winner is decided.
+        Currently 85 cards total
+        """
 
-		Read Spec - Peeks at a goal to check if correct or not
-		(Rethink Plan - Changes 2 of the 3 goals to different random ones.)
-		Add Line - Adds a line of code (or n characters)
-		Modify Line - Modifies a line of code (or < n characters)
-		Dead Battery - Traps a player
-		Disconnected WiFi - Traps a player
-		Stack Overflow Down - Traps a player (special). Everyone else has a 1/2 chance of skipping their turn after playing card.
-		Charger - Removes a Dead Battery trap
-		Internet Cafe - Removes a Disconnected Wifi trap
-		Go to Soda - Removes a Dead Battery and a Disconnected Wifi trap 
-		Code Guide - Makes a single player immune to Stack Overflow Down effect
-		Stack Overflow Restored - Can not be skipped. Removes all effects of ongoing Stack Overflow Down.
-		(Compile/Run) - Runs the code. After 3? run cards are played total, the game ends and winner is decided.
-		Currently 85 cards total
-		"""
-		self.cards = ["Read Spec"] * 5 \
-		+ ["Add Line"] * 21 \
-		+ ["Modify Line"] * 12 \
-		+ ["Dead Battery"] * 5 \
-		+ ["Disconnected WiFi"] * 5 \
-		+ ["Stack Overflow Down"] * 3\
-		+ ["Charger"] * 5 \
-		+ ["Internet Cafe"] * 5 \
-		+ ["Go to Soda"] * 5 \
-		+ ["Code Guide"] * 5 \
-		+ ["Stack Overflow Restored"] * 3 \
-		+ ["Run Code"] * 11
-		random.shuffle(self.cards)
+        self.cards = ["Read Spec"] * 5 \
+        + ["Add Line"] * 21 \
+        + ["Modify Line"] * 12 \
+        + ["Dead Battery"] * 5 \
+        + ["Disconnected WiFi"] * 5 \
+        + ["Stack Overflow Down"] * 3\
+        + ["Charger"] * 5 \
+        + ["Internet Cafe"] * 5 \
+        + ["Go to Soda"] * 5 \
+        + ["Code Guide"] * 5 \
+        + ["Stack Overflow Restored"] * 3 \
+        + ["Run Code"] * 11
 
-	def draw(self):
-		return self.cards.pop()
+        self.discard = []
 
-	def discard(self, card):
-		self.discard += list(card)
+        random.shuffle(self.cards)
 
-	def view_discard(self):
-		return self.discard
+    def draw(self):
+        return self.cards.pop()
 
-		
+    def discard(self, card):
+        self.discard += list(card)
+
+    def view_discard(self):
+        return self.discard
+
+        
 
 
 
--- game.py
+++ game.py
@@ -4,14 +4,22 @@ import sys
 import os
 import random
 import time
+from deck import Deck
 
 NUM_SABOTEURS = {3: 1, 4: 1, 5: 2, 6: 2, 7: 3, 8: 3, 9: 3, 10: 4}
 GOALS = [] #COMPLETE
 
 def main():
+    DEBUG = False
+
     # GET NUMBER OF PLAYERS
     if len(sys.argv) > 1:
-        num_players = int(sys.argv[-1])
+        if sys.argv[-1] == "-t":
+            DEBUG = True
+        if DEBUG:
+            num_players = int(sys.argv[-2])
+        else:
+            num_players = int(sys.argv[-1])
         current_player = 1
 
         print("There are %d players" % num_players)
@@ -20,7 +28,7 @@ def main():
         return
 
     # SET UP PLAYERS AND GAME ENVIRONMENT
-    goal = [] #random 3 from goals
+    goal = [] # random 3 from goals
     deck = Deck()
     players = list(range(1, num_players + 1))
     player_cards = {}
@@ -36,28 +44,44 @@ def main():
         if role == "saboteur":
             saboteurs.append(player)
 
-    # TELL WHO IS DA SABOTEUR and draw the cards
-    for player in players:
-        time.sleep(1)
-        os.system("clear")
-        time.sleep(1)
-        input("If you are player %d, press Enter" % player)
-        if player in saboteurs:
-            print("You are a SABOTEUR")
-        else:
-            print("You are a MINER")
         player_conditions[player] = []
         player_hand = []
         while len(player_hand) < 5:
             player_hand.append(deck.draw())
         player_cards[player] = player_hand
 
+    # TELL WHO IS DA SABOTEUR
+    if not DEBUG:
+        time.sleep(1)
+        for player in players:
+            os.system("clear")
+            time.sleep(1)
+            input("If you are player %d, press Enter" % player)
+            if player in saboteurs:
+                print("You are a SABOTEUR")
+            else:
+                print("You are a MINER")
+
+            time.sleep(1)
+            input("Press Enter to continue")
+
+
+        os.system("clear")
+        time.sleep(1)
+        print("GAME STAAAART")
+        time.sleep(3)
+
     # DO GAME
     while True: # game loop
-        print("Current player: Player %d" % current_player)
-        print(player_cards[player])
+        os.system("clear")
+        time.sleep(1)
+        input("If you are player %d, press Enter" % current_player)
 
-        pass
+        print("Your cards: %s" % ", ".join(player_cards[current_player]))
+
+        time.sleep(1)
+        input("Press Enter to continue")
+        current_player = current_player % num_players + 1
 
 
 if __name__ == "__main__":
